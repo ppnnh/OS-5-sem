6e82d230fb40a0e4f2b746d34702d9ab26d47712
lerochka <76012407+ppnnh@users.noreply.github.com>
2022-12-28 00:10:57 +0300

    Add files via upload

---
+++ lab6/OS06_03/os06_03.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+#define PG (4096)
+int pg = 512;
+
+int main() {
+	setlocale(LC_ALL, "rus");
+	int* m1 = (int*)VirtualAlloc(NULL, pg * PG, MEM_COMMIT, PAGE_READWRITE);
+	for (int i = 0; i < pg * PG / 4; i++) {
+		m1[i] = i;
+	}
+	system("pause");
+
+	// Б=C1=193
+	// у=F3=243
+	// р=F0=240
+	// Страница C1 = 193, смещение F3F=3903
+
+	int* m2 = m1 + 4 * 207 * 1024 + 3822;
+	cout << *m2 << endl << hex << m2 << endl;
+	system("pause");
+}
---
+++ lab6/OS06_04/OS06_04.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+void sh(HANDLE pheap)
+{
+    PROCESS_HEAP_ENTRY phe;
+    phe.lpData = NULL;
+    DWORD size = 0;
+    //TODO: Size of the heap
+    while (HeapWalk(pheap, &phe))
+    {
+        size += phe.cbData;
+        if (phe.wFlags & PROCESS_HEAP_UNCOMMITTED_RANGE)
+        {
+            cout << "-- address = " << hex << phe.lpData
+                << ", size = " << dec << phe.cbData << " U" << "\n";;// нераспределенная область
+        }
+        else if (phe.wFlags & PROCESS_HEAP_ENTRY_BUSY)
+        {
+            cout << "-- address = " << hex << phe.lpData
+                << ", size = " << dec << phe.cbData << " B" << "\n";;// распределенная область
+        }
+
+    }
+    std::cout << "size of the heap: " << size << endl;
+    std::cout << "-----------------------------------\n\n";
+}
+
+int main()
+{
+    HANDLE pheap = GetProcessHeap();
+
+    sh(pheap);
+    {
+        int size = 300000;
+        int* m = new int[size];
+        cout << "-- m = " << hex << m << " \n\n";
+    }
+    sh(pheap);
+    system("pause");
+}
